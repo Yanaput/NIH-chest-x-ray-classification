dc229539f40d3f72b976f5b3912b3679b1561138
Yanaput <nokg957@gmail.com>
2025-12-05 18:07:50 +0000

    Add test script

--- scripts/test.py
+++ scripts/test.py
@@ -14,24 +14,20 @@ from model.dataloader import NIHDataModule
 from model.classifier import NIHClassifier
 
 def main():
-    parser = argparse.ArgumentParser(description="Train NIH Chest X-ray Classifier")
+    parser = argparse.ArgumentParser()
     parser.add_argument("--train_csv", type=str, help="Path to train CSV", default="data/train.csv")
     parser.add_argument("--val_csv", type=str, help="Path to validation CSV", default="data/val.csv")
     parser.add_argument("--test_csv", type=str, help="Path to test CSV", default="data/test.csv")
-    parser.add_argument("--image_dir", type=str, help="Path to image directory (optional cache)", default="data/processed/images_all") 
+    parser.add_argument("--image_dir", type=str, help="Path to image directory", default="data/processed/images_all") 
+    parser.add_argument("--checkpoint", type=str, required=True, help="Path to model checkpoint")
     parser.add_argument("--batch_size", type=int, default=64, help="Batch size")
-    parser.add_argument("--epochs", type=int, default=70, help="Number of epochs")
-    parser.add_argument("--lr", type=float, default=1e-4, help="Learning rate")
     parser.add_argument("--num_workers", type=int, default=8, help="Number of dataloader workers")
     parser.add_argument("--img_size", type=int, default=256, help="Image size")
     parser.add_argument("--trans_crop", type=int, default=224)
     parser.add_argument("--log_dir", type=str, default="logs", help="Directory for logs")
-    parser.add_argument("--ckpt_dir", type=str, default="checkpoints", help="Directory for checkpoints")
-    parser.add_argument("--fast_dev_run", action="store_true", help="Run a quick development run")
     
     args = parser.parse_args()
 
-    # DataModule
     dm = NIHDataModule(
         train_csv=args.train_csv,
         val_csv=args.val_csv,
@@ -42,41 +38,25 @@ def main():
         num_workers=args.num_workers,
         cached_dir=args.image_dir
     )
+    # try:
+    #     pos_weight = NIHDataset.compute_pos_weight(args.train_csv)
+    #     print("Computed positive weights for class imbalance handling.")
+    #     print(pos_weight)
+    # except Exception as e:
+    #     print(f"Warning: Could not compute positive weights: {e}")
+    #     pos_weight = None
     backbone = DenseNet(num_classes=14, in_channels=1)
-
-    try:
-        pos_weight = NIHDataset.compute_pos_weight(args.train_csv)
-        print("Computed positive weights for class imbalance handling.")
-        print(pos_weight)
-    except Exception as e:
-        print(f"Warning: Could not compute positive weights: {e}")
-        pos_weight = None
-
-    model = NIHClassifier(model=backbone, pos_weight=pos_weight, lr=args.lr)
-    checkpoint_callback = ModelCheckpoint(
-        dirpath=args.ckpt_dir,
-        filename="{epoch}-{val_auc:.4f}",
-        monitor="val_auc",
-        mode="max",
-        save_top_k=3,
-        # save_last=True,
-    )
-    
-    lr_monitor = LearningRateMonitor(logging_interval="epoch")
-
-    logger = TensorBoardLogger(save_dir=args.log_dir, name="nih_classifier")
+    model = NIHClassifier.load_from_checkpoint(args.checkpoint, model=backbone, strict=False)
+    logger = TensorBoardLogger(save_dir=args.log_dir, name="nih_classifier_test")
 
     trainer = pl.Trainer(
-        max_epochs=args.epochs,
         accelerator="auto",
         devices="auto",
         logger=logger,
-        callbacks=[checkpoint_callback, lr_monitor],
-        fast_dev_run=args.fast_dev_run,
-        log_every_n_steps=100
     )
 
-    trainer.fit(model, datamodule=dm)
+    results = trainer.test(model, datamodule=dm)
+    print(f"Results: {results}")
 
 if __name__ == "__main__":
     main()
